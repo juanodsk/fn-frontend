d20adbdf58a2aa01b3a4c1cbc62dcc528b6f7766
juanodsk <juanph.dv@gmail.com>
2025-06-05 21:31:29 -0500

    commit base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:
-    "https://filadelfia-neiva-backend-bjv5kh-888cc4-165-227-180-87.traefik.me:3000",
+    "https://filadelfia-neiva-backend-bjv5kh-888cc4-165-227-180-87.traefik.me",
   withCredentials: true,
 });
 
