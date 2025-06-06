a4c0373a65614fb2529036268ae147407009af36
juanodsk <juanph.dv@gmail.com>
2025-06-05 21:16:28 -0500

    cambios url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL:
+    "https://filadelfia-neiva-backend-bjv5kh-888cc4-165-227-180-87.traefik.me:3000",
   withCredentials: true,
 });
 
